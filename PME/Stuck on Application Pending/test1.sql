ddac99eae8228db5801601f1e2b41182073a0021
Shashi-Tej-Reddy-Singa-Reddy <shashitejreddysingareddy@gmail.com>
2025-05-20 18:38:42 +0530

    d

---
+++ PME/Stuck on Application Pending/test1.sql
@@ -0,0 +1,9 @@
+use ScreeningConfiguration
+select * from externalservicelog
+where apgid = 52 and siteID = 5002374 and Source = 'UNIFIEDCRIMINAL'
+
+
+{"ConsumerId":null,"TransactionId":null,"CriminalFilterResponse":null,"ErrorDetails":{"Status":404,"Message":"No records found for ConsumerId - d3b1b515-06df-4c97-b9ea-7058649826c6 | TransactionId - bb3152c6-9d10-439e-9cea-e88b3612e6e8 | OrderName - Criminal"}}
+
+External ID : 
+bb3152c6-9d10-439e-9cea-e88b3612e6e8
