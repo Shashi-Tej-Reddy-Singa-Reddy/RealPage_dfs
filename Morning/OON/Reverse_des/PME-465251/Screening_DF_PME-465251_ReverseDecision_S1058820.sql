5ff9c3ae9172e7ca0338671f125ec4c9287007ba
Shashi-Tej-Reddy-Singa-Reddy <shashitejreddysingareddy@gmail.com>
2025-08-28 19:29:20 +0530

    s

---
+++ Morning/OON/Reverse_des/PME-465251/Screening_DF_PME-465251_ReverseDecision_S1058820.sql
@@ -0,0 +1,122 @@
+/*
+
+Description of the Problem: Reverse decision to no decision data fix.
+File Name:  Screening_DF_PME-465251_ReverseDecision_S1058820
+
+
+
+*/
+
+PRINT '******************** DATAFIX STARTED:  ********************'
+PRINT 'List the applicants'
+
+USE S1058820
+SELECT appID, apgID, appFirstName, appLastName, appTransmitDate, appBirthDate
+FROM ScreeningApplicant
+WHERE ApGId in(43006)
+
+
+
+PRINT 'List the screeningxglog'
+	SELECT *
+	FROM screeningxglog
+	WHERE (appid in(61557) or AppGID in (43006))
+	AND errorxml is not null
+	ORDER BY appid
+
+
+PRINT 'Display all Decision Types as a reference'
+SELECT * FROM ScreeningXgDecisionType
+
+PRINT 'BEFORE: List the Decision'
+SELECT *
+FROM ScreeningXgDecision  WHERE ApGId in (43006)
+
+PRINT 'Update:Reverse decision'
+Update screeningXgDecision
+set dtID = 0,deccode = 0
+where DecID = 45413 and apgid = 43006
+
+PRINT 'after: List the Decision'
+SELECT *
+FROM ScreeningXgDecision  WHERE ApGId in (43006)
+
+
+PRINT 'BEFORE:  List the decision information in the screening applicant group'
+SELECT apgid
+   ,codeDispositionCode
+   ,apgFinalBit
+   ,apgDispositionModifyUser
+   ,apgDispositionModifyDate
+   ,apgstatus
+   ,appStateID
+   ,codeDispositionCodeOverride
+   ,apgDispositionModifyUserOverride
+   ,apgDispositionModifyDateOverride
+FROM ScreeningApplicantGroup
+WHERE ApGId in (43006) 
+
+
+PRINT 'Update the information to the result received'
+--uppon checking this ensure that the final 3 result columns are null, if not replace with a null value.
+UPDATE ScreeningApplicantGroup
+set codeDispositionCode = NULL
+   ,apgFinalBit = 0
+   ,apgDispositionModifyUser= NULL
+   ,apgDispositionModifyDate= NULL
+   ,apgstatus ='REC'
+   ,appStateID = 2
+  ,codeDispositionCodeOverride= NULL
+  ,apgDispositionModifyUserOverride= NULL
+  ,apgDispositionModifyDateOverride= NULL
+where apgid in (43006)
+
+
+PRINT 'AFTER:  List the decision information in the screening applicant group'
+SELECT apgid
+   ,codeDispositionCode
+   ,apgFinalBit
+   ,apgDispositionModifyUser
+   ,apgDispositionModifyDate
+   ,apgstatus
+   ,appStateID
+   ,codeDispositionCodeOverride
+   ,apgDispositionModifyUserOverride
+   ,apgDispositionModifyDateOverride
+FROM ScreeningApplicantGroup
+WHERE ApGId in (43006)
+
+PRINT 'BEFORE:  List the asrResult in the AppScreeningResults Table'
+SELECT *
+FROM appscreeningresults
+WHERE asrAppGID in (43006)
+
+
+Print'Update screening results to not completed'
+Update appscreeningresults
+set asrresult = 0
+where asrappgid = 43006
+
+PRINT 'After:  List the asrResult in the AppScreeningResults Table'
+SELECT *
+FROM appscreeningresults
+WHERE asrAppGID in (43006)
+
+PRINT 'Display the activity log - Before'
+SELECT * FROM screeningactivitylog
+WHERE apgid in (43006)
+
+
+PRINT 'Insert activity entry concerning the datafix can be submitted.'
+PRINT ' '
+INSERT INTO  Screeningactivitylog (apgID, actDate, codeActivityTypeCode, actDescription, actUserID,  Displaybit, ModifyDate, leaID, GuestcardID,OSSiteID)
+VALUES ('43006', GETDATE(), 'SUBMIT', 'datafix - Reverse decision to no decision.', '1', '1', GETDATE(),'116955', NULL,'4380735')
+
+
+PRINT 'Display the activity log - After'
+PRINT ' '
+SELECT * 
+FROM screeningactivitylog
+WHERE apgid in (43006)
+
+PRINT '******************** DATAFIX COMPLETED:   ********************'
