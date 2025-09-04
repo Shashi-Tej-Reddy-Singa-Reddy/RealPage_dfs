57e052be0ac6c92823ce7ae6f07cdc80f176e5ef
Shashi-Tej-Reddy-Singa-Reddy <shashitejreddysingareddy@gmail.com>
2025-09-04 19:40:33 +0530

    a

---
+++ PME/Stuck on Pre-Screen/PME-465782/Screening_DF_S5516980_PQREC_PME-465782_1.sql
@@ -0,0 +1,55 @@
+
+/*
+Description of the Problem : Stuck on Pre-Screen Result: No Option to Resubmit or Rescore and LDS Score NA.
+File Name:  Screening_DF_S5516980_PQREC_PME-465782
+
+PME-465782
+
+*/
+
+use s5516980
+
+
+PRINT 'BEFORE:  List the decision information in the screening applicant group'
+SELECT apgid
+   ,codeDispositionCode
+   ,apgFinalBit
+   ,apgDispositionModifyUser
+   ,apgDispositionModifyDate
+   ,apgstatus
+   ,appStateID
+   ,codeDispositionCodeOverride
+   ,apgDispositionModifyUserOverride
+   ,apgDispositionModifyDateOverride
+FROM ScreeningApplicantGroup
+WHERE ApGId in (11) 
+
+
+
+PRINT 'Update the information to the result received'
+UPDATE ScreeningApplicantGroup
+set codeDispositionCode = NULL
+   ,apgFinalBit = 0
+   ,apgDispositionModifyUser= NULL
+   ,apgDispositionModifyDate= NULL
+   ,apgstatus ='REC'
+   ,appStateID = 2
+  ,codeDispositionCodeOverride= NULL
+  ,apgDispositionModifyUserOverride= NULL
+  ,apgDispositionModifyDateOverride= NULL
+where apgid in (11)
+
+
+PRINT 'AFTER:  List the decision information in the screening applicant group'
+SELECT apgid
+   ,codeDispositionCode
+   ,apgFinalBit
+   ,apgDispositionModifyUser
+   ,apgDispositionModifyDate
+   ,apgstatus
+   ,appStateID
+   ,codeDispositionCodeOverride
+   ,apgDispositionModifyUserOverride
+   ,apgDispositionModifyDateOverride
+FROM ScreeningApplicantGroup
+WHERE ApGId in (11)
