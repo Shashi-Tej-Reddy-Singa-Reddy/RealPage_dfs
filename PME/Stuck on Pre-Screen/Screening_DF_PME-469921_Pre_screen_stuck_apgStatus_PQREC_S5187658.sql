b8604adeb479cda425f1834fd915361c1256ab45
Shashi-Tej-Reddy-Singa-Reddy <shashitejreddysingareddy@gmail.com>
2025-09-25 00:29:07 +0530

    x

---
+++ PME/Stuck on Pre-Screen/Screening_DF_PME-469921_Pre_screen_stuck_apgStatus_PQREC_S5187658.sql
@@ -0,0 +1,71 @@
+/*
+Description of the Problem:  Stuck in Prescreen-LDS shows NA apgStatus PQREC
+
+File Name:  Screening_DF_PME-469921_Pre_screen_stuck_apgStatus_PQREC_S5187658
+
+
+PME-469921
+
+
+
+*/
+
+
+PRINT '******************** DATAFIX STARTED:  ********************'
+PRINT '  '
+USE s5187658
+
+
+PRINT 'BEFORE:  List the decision information in the screening applicant group'
+PRINT '  '
+SELECT apgid
+   ,codeDispositionCode
+   ,apgFinalBit
+   ,apgDispositionModifyUser
+   ,apgDispositionModifyDate
+   ,apgstatus
+   ,appStateID
+   ,codeDispositionCodeOverride
+   ,apgDispositionModifyUserOverride
+   ,apgDispositionModifyDateOverride
+FROM ScreeningApplicantGroup
+WHERE ApGId = 30725
+PRINT '  '
+
+PRINT 'Update the information to the result received'
+--uppon checking this ensure that the final 3 result columns are null, if not replace with a null value.
+UPDATE ScreeningApplicantGroup
+set codeDispositionCode = NULL
+   ,apgFinalBit = 0
+   ,apgDispositionModifyUser= NULL
+   ,apgDispositionModifyDate= NULL
+   ,apgstatus ='REC'
+   ,appStateID = 2
+  ,codeDispositionCodeOverride= NULL
+  ,apgDispositionModifyUserOverride= NULL
+  ,apgDispositionModifyDateOverride= NULL
+where apgid in (30725)
+
+
+
+PRINT 'AFTER:  List the decision information in the screening applicant group'
+PRINT '  '
+
+SELECT apgid
+   ,codeDispositionCode
+   ,apgFinalBit
+   ,apgDispositionModifyUser
+   ,apgDispositionModifyDate
+   ,apgstatus
+   ,appStateID
+   ,codeDispositionCodeOverride
+   ,apgDispositionModifyUserOverride
+   ,apgDispositionModifyDateOverride
+FROM ScreeningApplicantGroup
+WHERE ApGId = 30725
+PRINT '  '
+PRINT '  '
+
+
+
+PRINT '******************** DATAFIX COMPLETED:   ********************'
