7e5bf00687fa417022a42432d86d8bd87d36cbdb
Shashi-Tej-Reddy-Singa-Reddy <shashitejreddysingareddy@gmail.com>
2025-12-04 23:21:36 +0530

    h

--- Morning/Criminal Synergies/SQLQuery1.sql
+++ Morning/Criminal Synergies/SQLQuery1.sql
@@ -75,7 +75,7 @@ and source like '%CRIMINAL%'
 
 -- new
 USE ScreeningConfiguration
-select * from externalservicelog where apgid='82286' and siteid='4093192'
+select * from externalservicelog where apgid='78' and siteid='5505938'
 and source like '%CRIMINAL%'
 
 
