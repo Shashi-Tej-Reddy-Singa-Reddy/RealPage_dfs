e304d09a34be7aea164888f864ba3dcc689b7e41
Shashi-Tej-Reddy-Singa-Reddy <shashitejreddysingareddy@gmail.com>
2026-01-20 23:38:55 +0530

    s

---
+++ PME/Removal of Dispute/Screening_DF_Screeningconfiguration_RemoveCrimDispute_PME-489126.sql
@@ -0,0 +1,23 @@
+/*
+1lstr
+Description:  Remove from Criminal Dispute monitor
+File: Screening_DF_Screeningconfiguration_RemoveCrimDispute_PME-489126
+PME-489126
+
+This has no criminal records on the screening report and needs to be removed from the criminal monitor queue.
+*/
+PRINT '******************** DATAFIX STARTED:  ********************'
+USE ScreeningConfiguration
+PRINT 'Before:  Find the dispute where it can no longer be completed due to filtering'
+SELECT cdStatus, * FROM CriminalDisputes  WITH(NOLOCK)
+WHERE appid = '532' AND apgID =  '427'AND cdSiteID = '5486766' and cdEntityID = '1183013' --entityId == PMCId
+PRINT '  '
+PRINT 'UPDATE: the cdStatus to completed which is the value of 2, so it will no longer show in the dispute queue'
+UPDATE dbo.CriminalDisputes
+SET cdStatus = '2'
+WHERE appid = '532' AND apgID =  '427'AND cdSiteID = '5486766' and cdEntityID = '1183013' --entityId == PMCId
+PRINT '  '
+PRINT 'AFTER:  Find the dispute where it can no longer be completed due to filtering'
+SELECT cdStatus, * FROM CriminalDisputes  WITH(NOLOCK)
+WHERE appid = '532' AND apgID =  '427'AND cdSiteID = '5486766' and cdEntityID = '1183013' --entityId == PMCId
+PRINT '******************** DATAFIX COMPLETED ********************'
